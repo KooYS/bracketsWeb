8bc1599d2de941de593c17d055f82afb91b2b3f6
koo <tlfkthsl42@naver.com>
2017-08-16 20:16:13 +0900

    0816

---
+++ Getting Started/images/1_files/atom.php
@@ -0,0 +1,59 @@
+var OX_69fce3b7 = '';
+OX_69fce3b7 += "<"+"!DOCTYPE html>\n";
+OX_69fce3b7 += "<"+"html lang=\"ko\">\n";
+OX_69fce3b7 += "<"+"head>\n";
+OX_69fce3b7 += "<"+"meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\"/>\n";
+OX_69fce3b7 += "<"+"title>AdsByADOP<"+"/title>\n";
+OX_69fce3b7 += "<"+"style>\n";
+OX_69fce3b7 += "* { padding:0px; margin:0px; }\n";
+OX_69fce3b7 += "a { text-decoration:none; color:#313534; }\n";
+OX_69fce3b7 += "div { text-align: left; }\n";
+OX_69fce3b7 += "ul { list-style: none; }\n";
+OX_69fce3b7 += ".bn_box { width:200px; height:200px; position:relative; overflow:hidden; z-index:2000;}\n";
+OX_69fce3b7 += ".btn_logo { width:93px; height:15px; background-color:rgba(255, 255, 255, 0.7); padding:0px; margin:0px; color:#313534; position:absolute; top:0px; right:-78px; }\n";
+OX_69fce3b7 += ".btn_logo li { padding:0px; margin:0px; height:15px; line-height: 15px;}\n";
+OX_69fce3b7 += ".btn_logo li span { display:inline-block; height:15px; line-height:15px; font-size:12px; font-family:Sans-Serif; margin-top: -5px; vertical-align:middle; padding-left:2px; }\n";
+OX_69fce3b7 += ".btn_logo li span a { display:block; height:15px; line-height: 15px; vertical-align:middle; }\n";
+OX_69fce3b7 += "/* IE 10&11 */\n";
+OX_69fce3b7 += "@media screen and (-ms-high-contrast: active), (-ms-high-contrast: none) {\n";
+OX_69fce3b7 += ".btn_logo li span a { line-height: 8px; }\n";
+OX_69fce3b7 += "}\n";
+OX_69fce3b7 += "/* IE EDGE 12 */\n";
+OX_69fce3b7 += "@supports (-ms-accelerator:true) {\n";
+OX_69fce3b7 += ".btn_logo li span a { line-height: 8px; }\n";
+OX_69fce3b7 += "}\n";
+OX_69fce3b7 += "<"+"/style>\n";
+OX_69fce3b7 += "<"+"/head>\n";
+OX_69fce3b7 += "<"+"body style=\"padding:0; margin:0;\">\n";
+OX_69fce3b7 += "<"+"div class=\"bn_box\">\n";
+OX_69fce3b7 += "<"+"ul class=\"btn_logo\" style=\"right: -78px;\">\n";
+OX_69fce3b7 += "<"+"li><"+"img src=\"http://dsp.adop.cc/assets/img/adop/bn_adop.png\" alt=\"adop\"><"+"span><"+"a href=\"http://dsp.adop.cc/serving/l?r=http%3A%2F%2Fwww.adop.cc%2Fadop-contact\" target=\"_blank\">Ads by ADOP<"+"/a><"+"/span><"+"/li>\n";
+OX_69fce3b7 += "<"+"/ul>\n";
+OX_69fce3b7 += "<"+"a href=\"http://dsp.adop.cc/serving/c?u=37&g=68&c=74&cm=70&ta=71&i=98&ig=29&ar=9631&tp=120&pa=0&pf=24&pp=96&rg=11&r=http%3A%2F%2Flockplant.co.kr%2Fform6%2F%3Fpid%3Dadrcomms\" target=\"_blank\">\n";
+OX_69fce3b7 += "<"+"img src=\"http://atomcdn.adop.cc/adimage/6ea9ab1baa0efb9e19094440c317e21b/d9531365a909fd4128ca8317bfcf870a\" />\n";
+OX_69fce3b7 += "<"+"script src=\"http://atom-log-collector-884724588.ap-northeast-2.elb.amazonaws.com/atom/imp?u=37&g=68&c=74&cm=70&ta=71&i=98&ig=29&ar=9631&tp=120&pa=0&pf=24&pp=96&rg=11\" style=\"display:none;\"><"+"/script>\n";
+OX_69fce3b7 += "<"+"/a>\n";
+OX_69fce3b7 += "<"+"/div>\n";
+OX_69fce3b7 += "<"+"/body>\n";
+OX_69fce3b7 += "<"+"script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"><"+"/script>\n";
+OX_69fce3b7 += "<"+"script>\n";
+OX_69fce3b7 += "$(document).ready(function () {\n";
+OX_69fce3b7 += "$(\".btn_logo\").bind(\"mouseenter focus\", function () {\n";
+OX_69fce3b7 += "$(this).stop().animate({\"right\": \"0px\"});\n";
+OX_69fce3b7 += "}).bind(\"mouseleave blur\", function () {\n";
+OX_69fce3b7 += "$(this).stop().animate({\"right\": \"-78px\"});\n";
+OX_69fce3b7 += "});\n";
+OX_69fce3b7 += "});\n";
+OX_69fce3b7 += "<"+"/script>\n";
+OX_69fce3b7 += "<"+"/html>\n";
+
+            if(OX_69fce3b7 != '')
+            {
+            var iframe = document.write('<iframe id=\'OX_69fce3b7\' width=\'200px\' height=\'200px\' border=0 scrolling=no frameborder=0 ></iframe>');var ifrm = document.getElementById('OX_69fce3b7');
+    ifrm = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
+    ifrm.document.open();
+    ifrm.document.write(OX_69fce3b7);
+    ifrm.document.close();}
+
+
+
