e20e6b167b5379ce056d8ba0ad6eb9f655904e99
koo <tlfkthsl42@naver.com>
2017-08-21 00:22:09 +0900

    0820

---
+++ Getting Started/images/1_files/ajs(1).php
@@ -0,0 +1,8 @@
+(function(){
+var s = "<"+"div id=\'beacon_f8af37be6f\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"http://cat.jp.as.criteo.com/delivery/lg.php?cppv=1&cpp=1STWinxsQWMzbTltYkd0OGxidU9hVU1CWjZLeGpRVHZqNWJjRXRyNXNVR0lFSVhTKzk1aWk2aGlUcTBnSEJuSkt6R1NSYlIvTHBLK2xrNVZpVFVURXZLU0s3aXJkTkJNWVRaa09LYXVsL1FTTzdnTXJEbVlyRzVBenJ4YW9NUFFCSy93MzNvWHQ5NHUvWUNGcUlpNmNtaTRETloybEpKYmpvZ1ZmUVQ3RGU3WFBTeFBNaG5DYzhXUzJkTEIyLzJJQ3lqQkhUdnRia0txdkdqNXhPZ2o2Z2tkd2x5NUU1NVY4WiswTkdwNCtHUmQzdDZ3b0VkWXo4dmY3VW16czlHYWovRnNafA%3D%3D\"/>\n";
+s += "<"+"/div>\n";
+s += "<"+"iframe src=\"http://adex.ednplus.com/xc/h/Fz55CjWZ/passback?r=798583967&xcb=4\" width=\"200\" height=\"200\" frameborder=\"0\" scrolling=\"no\" topmargin=\"0\" leftmargin=\"0\" marginwidth=\"0\" marginheight=\"0\"><"+"/iframe>\n";
+s += "\n";
+s += "\n";
+document.write(s);})();
